845357842252911f07f71eb53ea77399b2b37098
uae <aniekaudo@yahoo.com>
2023-06-26 10:14:39 +0100

    :art: Footer
    Set up structure frfor footer

--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -16,10 +16,9 @@ const NavbarMain = () => {
           TRYDOT SYNERGY
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
+        <Navbar.Collapse id="navbarScroll" className={styles.navbar_collapse}>
           <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px', marginLeft: 'auto' }}
+            className={`me-auto my-2 my-lg-0 ${styles.Nav}`}
             navbarScroll
           >
             <Nav.Link href="#action1" className={styles.nav_link}>
@@ -29,17 +28,66 @@ const NavbarMain = () => {
               title="Pages"
               id={`navbarScrollingDropdown ${styles.nav__link}`}
               className={styles.nav_link}
-              style={{ color: 'red' }}
             >
-              <NavDropdown.Item href="#action3" className={styles.nav_link_item}>
-                Action
+              <NavDropdown.Item
+                href="#action3"
+                className={styles.nav_link_item}
+              >
+                About Us
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action4" className={styles.nav_link_item}>
-                Another action
+              <NavDropdown.Item
+                href="#action4"
+                className={styles.nav_link_item}
+              >
+                Contact Us
               </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5" className={styles.nav_link_item}>
-                Something else here
+              <NavDropdown.Item
+                href="#action5"
+                className={styles.nav_link_item}
+              >
+                Faq&apos;s
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href="#action5"
+                className={styles.nav_link_item}
+              >
+                Agents
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href="#action5"
+                className={styles.nav_link_item}
+              >
+                Testimonials
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href="#action5"
+                className={styles.nav_link_item}
+              >
+                Service Box
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href="#action5"
+                className={styles.nav_link_item}
+              >
+                Partners
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href="#action5"
+                className={styles.nav_link_item}
+              >
+                Services
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href="#action5"
+                className={styles.nav_link_item}
+              >
+                Single Service
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                href="#action5"
+                className={styles.nav_link_item}
+              >
+                404
               </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown
@@ -48,15 +96,17 @@ const NavbarMain = () => {
               className={styles.nav_link}
               style={{ color: 'red' }}
             >
-              <NavDropdown.Item href="#action3" className={styles.nav_link_item}>
-                Action
+              <NavDropdown.Item
+                href="#action3"
+                className={styles.nav_link_item}
+              >
+                Portfolio List
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action4" className={styles.nav_link_item}>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5" className={styles.nav_link_item}>
-                Something else here
+              <NavDropdown.Item
+                href="#action4"
+                className={styles.nav_link_item}
+              >
+                Single Project
               </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown
@@ -65,45 +115,49 @@ const NavbarMain = () => {
               className={styles.nav_link}
               style={{ color: 'red' }}
             >
-              <NavDropdown.Item href="#action3" className={styles.nav_link_item}>
-                Action
+              <NavDropdown.Item
+                href="#action3"
+                className={styles.nav_link_item}
+              >
+                Blog
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action4" className={styles.nav_link_item}>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5" className={styles.nav_link_item}>
-                Something else here
+              <NavDropdown.Item
+                href="#action4"
+                className={styles.nav_link_item}
+              >
+                Single Post
               </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown
               title="Shop"
               id={`navbarScrollingDropdown ${styles.nav__link}`}
               className={styles.nav_link}
-              style={{ color: 'red' }}
             >
-              <NavDropdown.Item href="#action3" className={styles.nav_link_item}>
-                Action
+              <NavDropdown.Item
+                href="#action3"
+                className={styles.nav_link_item}
+              >
+                Shop
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action4" className={styles.nav_link_item}>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5" className={styles.nav_link_item}>
-                Something else here
+              <NavDropdown.Item
+                href="#action4"
+                className={styles.nav_link_item}
+              >
+                Single Product
               </NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="#" disabled className={styles.nav_link}>
-             Elements
+              Elements
             </Nav.Link>
           </Nav>
-        
-            <Button variant="outline-success" className={styles.navBtn}>+2347036471219</Button>
-        
+
+          <Button variant="outline-success" className={styles.navBtn}>
+            +2347036471219
+          </Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default NavbarMain;
